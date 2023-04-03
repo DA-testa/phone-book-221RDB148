3bc7b3ad93c8c3ac0ada4e56a1a928e459b2b887
221RDB148 <valters.martinsons@edu.rtu.lv>
2023-04-03 04:26:57 +0300

    working

--- main.py
+++ main.py
@@ -28,11 +28,44 @@ def get_prime(i):
         n = n+1
 
 def read_queries():
-    # n = int(input())
-    # return [Query(input().split()) for i in range(n)]
-    return [Query(["add",123,"Bob"]),Query(["add",321,"Obo"]),Query(["add",222,"SDS"]),Query(["find",321])]
+    n = int(input())
+    return [Query(input().split()) for i in range(n)]
+######################################################
+#     n = 12
+#     s = """add 911 police
+# add 76213 Mom
+# add 17239 Bob
+# find 76213
+# find 910
+# find 911
+# del 910
+# del 911
+# find 911
+# find 76213
+# add 76213 daddy
+# find 76213"""
+#     lines = s.splitlines()
+#     # queries = [Query(line.split()) for line in lines]
+#     queries = [[] for i in range(n)]
+#     k=0
+#     for i in lines:
+#         queries[k] = i
+#         k=k+1
+#######################################################
+    # return [Query(["add",123,"Bob"]),
+    #         Query(["add",321,"Obo"]),
+    #         Query(["add",222,"SDS"]),
+    #         Query(["find",321]),
+    #         Query(["add",523,"vdf"]),
+    #         Query(["add",532,"bfj"]),
+    #         Query(["add",660,"ged"]),
+    #         Query(["add",896,"vdv"]),
+    #         Query(["find",660]),
+    #         Query(["find",000]),]
+
 
 def write_responses(result):
+    print()
     print('\n'.join(result))
 
 def process_queries(queries):
@@ -41,10 +74,11 @@ def process_queries(queries):
     for cur_query in queries:
         if cur_query.type == 'add':
             size = size + 1
-
+    
+    print(size)
     # declare emply hash table
     hash_table = [[] for i in range(size)]
-
+    
     # nitialising hash function variables
     p = get_prime(size)
     a = random.randint(1, p-1)
@@ -57,17 +91,12 @@ def process_queries(queries):
 
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-
-
     # contacts = [Cont(i.number, i.name) for i in queries]
     # contacts = []
     # contacts = [[i.number,i.name] for i in queries]
-
     # for i in contacts:
     #     print(i)
 
-    
-        
     for cur_query in queries:
         if cur_query.type == 'add':
             hashed_index = hash_function(cur_query.number)
@@ -76,34 +105,44 @@ def process_queries(queries):
             for i in hash_table[hashed_index]:
                 if i[0] == cur_query.number:
                     i[1] = cur_query.name
-                    return
+                    continue
             else: # otherwise, just add it
                 hash_table[hashed_index].append([cur_query.number,cur_query.name])
+                continue
         
 
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            hashed_index = hash_function(cur_query.number)
+            for i in hash_table[hashed_index]:
+                if cur_query.number in i:
+                    hash_table.remove[i]
+                    print("removed")
+                    continue
+
 
 
         elif cur_query.type == 'find':
             hashed_index = hash_function(cur_query.number)
+            k=0
             for i in hash_table[hashed_index]:
                 if i[0] == cur_query.number:
                     # return i[1]
                     print(i[1])
-                else:
-                    print("wasnt found")
+                    result.append(i[1])
+                    k=k + 1
+            if k==0: 
+                print("not found")
+                result.append("not found")
         
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+        # else:
+        #     response = 'not found'
+        #     print("not found")
+        #     for contact in contacts:
+        #         if contact.number == cur_query.number:
+        #             response = contact.name
+        #             break
+        #     result.append(response)
+        # print("not asdasd", cur_query.type)
     return result
 
 if __name__ == '__main__':
---
+++ test2.py
@@ -0,0 +1,11 @@
+list = [[1,2],[3,4],[5,6],[7,8]]
+
+ind = 1
+
+
+for i in list:
+    if 3 in i:
+        list.remove(i)
+        
+print(list)
+
